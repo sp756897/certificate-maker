9b2754d9cb87cb5e1a368a6c68536c11607c0a5f
sp756897 <akshaysp2090@gmail.com>
2021-09-01 23:36:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { white } from 'chalk';
 import 'minireset.css';
 import './App.css';
+import logo from "./S-rmbg.png"
 
-function App({ name, course, date }) {
+function App({ name1,name2, place,lovedate, txhash, txlink, date }) {
   return (
     <div className="App">
       <Icon />
-      <p className="byline">Certificate of completion</p>
-
+      <div className="row">
+      <p className="byline" style={{"color":"white"}}>Certificate of </p><p className="byline" style={{"color":"red"}}><b>Love</b></p>
+      </div>
+      
       <div className="content">
-        <p>Awarded to</p>
-        <h1>{name}</h1>
-        <p>for completing:</p>
-        <h2>{course}</h2>
+        <h1>{name1} Love {name2}</h1>
+        <p>Place of Love</p>
+        <h2>{place}</h2>
+        <p>Date of Love</p>
+        <h2>{lovedate}</h2>
+        <p>Trx Hash</p>
+        <h2>{txhash}</h2>
+        <p>Trx Link</p>
+        <h2>{txlink}</h2>
       </div>
 
       {date && (
@@ -25,15 +34,19 @@ function App({ name, course, date }) {
 }
 
 App.defaultProps = {
-  name: 'James Lee',
-  course: 'Creating PDFs with React & Make.cm',
+  name1: 'James Lee',
+  name2: 'James Lee',
+  place: 'Creating PDFs with React & Make.cm',
+  lovedate:"10-01-2021",
+  txhash:"sample",
+  txlink:"link",
   date: 'March 15 2021'
 }
 
 const Icon = () => (
-  <svg width="99" height="139" viewBox="0 0 99 139" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 0H99V138.406L52.1955 118.324L0 138.406V0Z" fill="white" />
-    <path d="M25.4912 83.2515C25.4912 79.4116 27.0222 75.7289 29.7474 73.0137C32.4727 70.2985 36.1689 68.7731 40.0229 68.7731C43.877 68.7731 47.5732 70.2985 50.2984 73.0137C53.0236 75.7289 54.5546 79.4116 54.5546 83.2515M40.0229 59.724C40.0229 55.8841 41.5539 52.2014 44.2791 49.4862C47.0044 46.7709 50.7006 45.2455 54.5546 45.2455C58.4087 45.2455 62.1049 46.7709 64.8301 49.4862C67.5553 52.2014 69.0863 55.8841 69.0863 59.724V83.2515" stroke="#0379FF" strokeWidth="10.6193" />
+  <svg class="heart" viewBox="0 0 32 29.6">
+    <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
   </svg>
 )
 
